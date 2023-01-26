864d1621c6d1f2f5ebf6e3cec3d17eaa845c801e
Eduardo Job <venonbrine@gmail.com>
2023-01-26 00:28:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMobileMenu(menu){
+    
+    menu.classList.toggle('open');
+}
+
+function exibeAnselmo(){
+    var div = document.getElementById("anselmo");
+    div.style.top = "12%";
+}
+function fechaAnselmo(){
+    var div = document.getElementById("anselmo");
+    div.style.top = "200%";
+}
