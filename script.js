8890dea8db2ba400d943a5403bf3bb8bf63e5d86
Eduardo Job <venonbrine@gmail.com>
2023-01-28 02:55:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,56 @@ function toggleMobileMenu(menu){
     menu.classList.toggle('open');
 }
 
+function exibeCesar(){
+    var div = document.getElementById("cesar");
+    div.style.top = "12%";
+}
+function fechacesar(){
+    var div = document.getElementById("cesar");
+    div.style.top = "200%";
+}
+
 function exibeAnselmo(){
     var div = document.getElementById("anselmo");
     div.style.top = "12%";
 }
 function fechaAnselmo(){
     var div = document.getElementById("anselmo");
+    div.style.top = "200%";
+}
+
+function exibedaniel(){
+    var div = document.getElementById("daniel");
+    div.style.top = "12%";
+}
+function fechadaniel(){
+    var div = document.getElementById("daniel");
+    div.style.top = "200%";
+}
+
+function exibefellipe(){
+    var div = document.getElementById("fellipe");
+    div.style.top = "12%";
+}
+function fechafellipe(){
+    var div = document.getElementById("fellipe");
+    div.style.top = "200%";
+}
+
+function exibemauricio(){
+    var div = document.getElementById("mauricio");
+    div.style.top = "12%";
+}
+function fechamauricio(){
+    var div = document.getElementById("mauricio");
+    div.style.top = "200%";
+}
+
+function exibepaulo(){
+    var div = document.getElementById("paulo");
+    div.style.top = "12%";
+}
+function fechapaulo(){
+    var div = document.getElementById("paulo");
     div.style.top = "200%";
 }
